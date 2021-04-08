70a411db5a9cc58e4fc1afcf91d0b76243c31f90
lhz123456 <229758024@qq.com>
2021-04-08 16:47:14 +0800

    提交

--- runAll.py
+++ runAll.py
@@ -178,11 +178,8 @@ class AllTest:  # 定义一个类AllTest
         读取caselist.txt文件中的用例名称，并添加到caselist元素组
         :return:
         """
-        print('等等'+self.caseListFile)
         fb = open(self.caseListFile,'r')
-        print('大佬')
         for value in fb.readlines():
-            print('zhizhiz')
             data = value
             if data != '' and not data.startswith("#"):  # 如果data非空且不以#开头
                 self.caseList.append(data.replace("\n", ""))  # 读取每行数据会将换行转换为\n，去掉每行数据中的\n
@@ -201,7 +198,6 @@ class AllTest:  # 定义一个类AllTest
             # 批量加载用例，第一个参数为用例存放路径，第一个参数为路径文件名
             discover = unittest.defaultTestLoader.discover(self.caseFile, pattern=case_name + '.py', top_level_dir=None)
             suite_module.append(discover)  # 将discover存入suite_module元素组
-            print('suite_module:' + str(suite_module))
         if len(suite_module) > 0:  # 判断suite_module元素组是否存在元素
             for suite in suite_module:  # 如果存在，循环取出元素组内容，命名为suite
                 for test_name in suite:  # 从discover中取出test_name，使用addTest添加到测试集
